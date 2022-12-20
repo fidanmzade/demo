9bd3bfe07e902ac0196c538396467bcd289d0f0f
fidanmzade <fidanm3zade@gmail.com>
2022-12-20 14:37:57 +0400

    film

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Routes,Route} from 'react-router-dom'
+import './index.css'
+import ListPage from './ListPage';
+import MainPage from './MainPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    return (
+      <div className="app">
+       <header className="header">
+                <h1 className="header__title">
+                     Movies & Films
+                </h1>
+            </header>
+      <Routes>
+      <Route path='' element={ <MainPage/>}/>
+      <Route path='/list' element={<ListPage />}/>
+      </Routes>   
+      </div>
+    );
+
 }
 
 export default App;
